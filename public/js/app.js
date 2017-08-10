dfa3beb54a8147930508191e1553f690cc1b200d
Joel Guerra & Kris <pair+joel-g.knweber@devbootcamp.com>
2017-08-09 17:28:47 -0700

    solved null lats and lons

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     populateMarkers(MAPAPP.pathName);
 });
 
+
 //Initialize our Google Map
 function initialize() {
     var center = new google.maps.LatLng(40.101, -96.62);
@@ -19,16 +20,13 @@ function initialize() {
     };
     this.map = new google.maps.Map(document.getElementById('map_canvas'),
         mapOptions);
+
 };
 
 // Fill map with markers
 function populateMarkers(dataType) {
     // apiLoc = typeof apiLoc !== 'undefined' ? apiLoc : '/data/' + dataType + '.json';
     // jQuery AJAX call for JSON
-
-
-
-
     $.getJSON('/data/policewatchdata.json', function(data) {
 
         $.each(data, function(i, ob) {
@@ -38,7 +36,14 @@ function populateMarkers(dataType) {
                 icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
             });
 
-            var content = 'Name: ' + this.name + '<br>Age: ' + this.age;
+            var content = 'Name: ' + this.name +
+                        '<br>Age: ' + this.age +
+                        '<br>Sex: ' + this.sex +
+                        '<br>Race: ' + this.race +
+                        '<br>Cause: ' + this.cause +
+                        '<br>Armed: ' + this.armed +
+                        '<br>Dept: ' + this.dept;
+
         	marker.infowindow = new google.maps.InfoWindow({
             	content: content,
             	maxWidth: 400
@@ -51,5 +56,7 @@ function populateMarkers(dataType) {
             });
             MAPAPP.markers.push(marker);
         });
+        var markerCluster = new MarkerClusterer(map, MAPAPP.markers,
+            {imagePath: '/images/m'});
     });
 };
